e7367d69079fd8fb5bd6b44d280c1e0eb83b138e
Wellington Santos <wellingtonstoz@gmail.com>
2023-05-08 21:09:45 -0300

    relogio digital commit

---
+++ Relógio Digital/script.js
@@ -0,0 +1,21 @@
+const horas = document.getElementById('horas');
+const minutos = document.getElementById('minutos');
+const segundos = document.getElementById('segundos');
+
+
+const relogio = setInterval(() => {
+    const date = new Date();
+  
+    let hr = date.getHours();
+    let min = date.getMinutes();
+    let sec = date.getSeconds();
+  
+    if(hr < 10) hr = '0' + hr;
+    if(min < 10) min = '0' + min;
+    if(sec < 10) sec = '0' + sec;
+  
+    horas.textContent = hr;
+    minutos.textContent = min;
+    segundos.textContent = sec;
+  });
+  
